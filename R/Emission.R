adc46eed73f5c49ecc7a3b0f088e6b7e9d296dd2
Jaak0v <jaakovlol@gmail.com>
2020-12-08 14:59:05 +0100

    Initial commit

---
+++ R/Emission.R
@@ -0,0 +1,42 @@
+#' Obliczenia emisji spalin
+#'
+#' @param dane dataframe
+#' @param kategoria character
+#' @param euro character
+#' @param mode character
+#' @param substancja character
+#'
+#' @return dataframe
+#' @import dplyr tidyverse ggplot2 magrittr
+#' @export
+#'
+#' @examples
+Emission <- function(dane = input,
+                     kategoria = "Passenger Cars",
+                     #paliwo = "Petrol",
+                     #segment = "Mini",
+                     euro = "Euro 5",
+                     #technologia = "",
+                     mode = "",
+                     substancja = c("CO", "EC")) {
+  out <- wskazniki %>%
+    filter(Category %in% kategoria) %>%
+    filter(Euro.Standard %in% euro) %>%
+    filter(Pollutant %in% substancja) %>%
+    filter(Mode %in% mode)
+  #filter(Fuel %in% paliwo)
+  #filter(Segment %in% segment)
+  
+  out <- inner_join(x = out, y = input, by = c("Segment","Fuel","Technology"))
+  
+  out <- na.omit(out) %>%
+    mutate(Emisja = Nat * ((Alpha * Procent ^ 2 + Beta * Procent + Gamma + (Delta/Procent))/
+                             (Epsilon * Procent ^ 2 + Zita * Procent + Hta) * (1-Reduction))
+    ) %>%
+    select(Category, Fuel, Euro.Standard, Technology, Pollutant, Mode, Segment, Emisja, Nat)
+  
+  out <<- out
+  return(out)
+}
+
+
---
+++ R/Emsplot.R
@@ -0,0 +1,25 @@
+#' Stworzenie wykresu
+#'
+#'
+#' @return plot
+#' @param x double
+#' @param y double
+#' @param out double
+#' @import dplyr tidyverse ggplot2 magrittr
+#' @export
+#'
+#' @examples
+
+Emsplot <- function(out,x,y,z) {
+  
+  x <- enquo(x)
+  y <- enquo(y)
+  z <- enquo(z)
+  
+  ggplot(out,aes(!!x, !!y, colour = !!z)) +
+    geom_point() +
+    theme_bw() -> p
+  
+  return(p)
+}
+
